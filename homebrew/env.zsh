2aa4ad748221830a51f8102017fdde7a23e6ca35
Chris Caller <chris.caller@googlemail.com>
2019-11-20 23:21:05 +0000

    new brews

--- homebrew/env.zsh
+++ homebrew/env.zsh
@@ -1,6 +1,6 @@
 export HOMEBREW_CASK_OPTS="--fontdir=~/Library/Fonts/Homebrew"
 
-# Source chtf
+# chtf (https://github.com/Yleisradio/homebrew-terraforms)
 if [[ -f /usr/local/opt/chtf/share/chtf/chtf.sh ]]; then
     source "/usr/local/opt/chtf/share/chtf/chtf.sh"
 fi
